ab968032ec05aef43b370740f7c93e663c14cd9e
Prodge <prodge@prodge.net>
2016-10-14 21:53:18 +0000

    Completed python implementation

---
+++ python/diamond_kata.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+'''
+
+    Diamond Kata by prodge
+
+'''
+
+
+import sys
+
+def letter_at_row(row):
+    return chr(ord('a') + row)
+
+def start_spaces(row, order):
+    return ' ' * (order - row)
+
+def middle_spaces(row, order):
+    return ' ' * (row * 2 - 1)
+
+def get_row(row, order):
+    if row == 0:
+        return ' ' * order + letter_at_row(row)
+    return start_spaces(row, order) + letter_at_row(row) + middle_spaces(row, order) + letter_at_row(row)
+
+def get_diamond_top(order):
+    return [get_row(row, order) for row in range(order + 1)]
+
+def get_diamond(order):
+    top = get_diamond_top(order)
+    return top + top[::-1][1:]
+
+def main(args):
+    assert len(args) == 2, "Please enter the deepest letter of the diamond"
+    letter = args[1]
+    order = ord(letter) - ord('a')
+    list(map(print, get_diamond(order)))
+
+main(sys.argv)

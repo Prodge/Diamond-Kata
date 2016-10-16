8c8fdc693b81b4a88869e83e6175ce8a3b96023e
Prodge <prodge@prodge.net>
2016-10-16 13:35:14 +0000

    Completed implementation

--- clojure3/src/clojure3/core.clj
+++ clojure3/src/clojure3/core.clj
@@ -1,7 +1,29 @@
 (ns clojure3.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn spaces [n]
+  (apply str (repeat n " ")))
+
+(defn row-letter [row]
+  (-> (int \a) (+ row) char str))
+
+(defn letter-at-pos [letter pos width]
+  (str (spaces pos) letter (spaces (- width pos))))
+
+(defn line-string [line order]
+  (let [corner (letter-at-pos (row-letter line) (- order line) order)]
+    (apply str corner (reverse (butlast corner)))))
+
+(defn get-diamond-top [order]
+  (loop [line 0 diamond []]
+    (if (<= line order)
+      (recur (inc line) (conj diamond (line-string line order)))
+      diamond)))
+
+(defn get-diamond [order]
+  (let [top (get-diamond-top order)]
+    (into top (reverse (butlast top)))))
+
+(defn -main [arg]
+  (let [order (-> arg first int (- (int \a)))]
+    (doall (map println (get-diamond order)))))
--- clojure3/test/clojure3/core_test.clj
+++ clojure3/test/clojure3/core_test.clj
@@ -2,6 +2,46 @@
   (:require [clojure.test :refer :all]
             [clojure3.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-spaces
+  (testing "The correct number of spaces is returned"
+    (is (= (spaces 0) ""))
+    (is (= (spaces 1) " "))
+    (is (= (spaces 2) "  "))
+    ))
+
+(deftest letter-at-pos-test
+  (testing "Getting a getting a string with a letter at the set pos"
+    (is (= (letter-at-pos "a" 0 4) "a    "))
+    (is (= (letter-at-pos "a" 1 4) " a   "))
+    (is (= (letter-at-pos "a" 2 4) "  a  "))
+    (is (= (letter-at-pos "a" 3 4) "   a "))
+    (is (= (letter-at-pos "a" 4 4) "    a"))
+    ))
+
+(deftest line-char-test
+  (testing "Getting a character at a given line"
+    (is (= (row-letter 0) "a"))
+    (is (= (row-letter 1) "b"))
+    (is (= (row-letter 2) "c"))
+    ))
+
+(deftest line-string-test
+  (testing "Generating the string to be printed on the given line"
+    (is (= (line-string 0 2)  "  a  "))
+    (is (= (line-string 1 2 ) " b b "))
+    (is (= (line-string 2 2)  "c   c"))))
+
+(deftest get-diamond-top-test
+  (testing "The collection for the top of the diamond"
+    (is (= (get-diamond-top 2) ["  a  "
+                                " b b "
+                                "c   c"]))))
+
+(deftest get-diamond-test
+  (testing "The collection for the final structure is valid"
+    (is (= (get-diamond 2)
+           ["  a  "
+            " b b "
+            "c   c"
+            " b b "
+            "  a  "]))))
